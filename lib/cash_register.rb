a73c833d2cc04ddd69e0a98702a62420472da23a
Alexander Clamor <alexander.clamor@gmail.com>
2020-08-26 09:42:51 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -44,7 +44,7 @@ attr_accessor :total, :discount, :items, :transactions
 
 end
 
-register = CashRegister.new(20)
+register = CashRegister.new
 register.add_item("beans",5.1)
 register.add_item("beans",2.0, 2.0)
 puts register.void_last_transaction
