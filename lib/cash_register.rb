d6037742cbb6260d211c346b34a1a963b45fab41
Alexander Clamor <alexander.clamor@gmail.com>
2020-08-26 08:39:12 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -10,7 +10,8 @@ attr_accessor :total, :discount, :items
   
   def add_item(title, price, quantity = 1)
     self.total += price * quantity
-    @items << title
+    quantity = 1 ? @items << title : @items << title * quantity
+    
   end
 
   def apply_discount
