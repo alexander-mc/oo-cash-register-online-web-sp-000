0bd0fbe15121f3bb16e10a7dd60b986de1fda253
Alexander Clamor <alexander.clamor@gmail.com>
2020-08-26 09:34:39 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -3,7 +3,7 @@ class CashRegister
 attr_accessor :total, :discount, :items, :last_transaction
 
   def initialize(discount = 0)
-    @total = 0
+    @total = 0.0
     @discount = discount
     @items = []
     @transactions = []
